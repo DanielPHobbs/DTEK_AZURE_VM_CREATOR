68b5a3884592a0ae5960f5be5516942c24083e75
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2019-02-23 08:29:55 +0000

    create

---
+++ SCSM AZUREDATA IMPORTER/AZURE_DATA_CSV/Import_CSV_SCSM.ps1
@@ -0,0 +1,7 @@
+Import-SCSMInstance -FormatFileName "computers.xml" -DataFileName "computers.csv" -BatchSize 100
+
+Import-SCSMInstance -FormatFileName "computers.xml" -DataFileName "computers.csv" -BatchSize 100
+
+Import-SCSMInstance -FormatFileName "computers.xml" -DataFileName "computers.csv" -BatchSize 100
+
+Import-SCSMInstance -FormatFileName "computers.xml" -DataFileName "computers.csv" -BatchSize 100
