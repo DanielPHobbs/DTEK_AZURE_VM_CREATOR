d5dba21ca24489da407adb161105c43ffaf47598
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2019-02-25 20:19:47 +0000

    edit

--- AZURE SCRIPTS/LoginAzureApp.ps1
+++ AZURE SCRIPTS/LoginAzureApp.ps1
@@ -12,5 +12,57 @@
 #>
 Install-Module Az –Repository PSGallery –AllowClobber
 
-$cred = New-Object System.Management.Automation.PSCredential ("02e79480-acfb-4284-ac9e-7caaa8b244e84", "z5KMwA3DmvoeeSCvzoocvq5pHi78gsFR+0iKoSDvHZk=")
-Connect-AzAccount -Credential $cred -ServicePrincipal -TenantId 92832cfc-349a-4b12-af77-765b6f10b51f
+
+
+
+$applicationId = "02e79480-acfb-4284-ac9e-7caaa8b244e8";
+$securePassword = "z5KMwA3DmvoeeSCvzoocvq5pHi78gsFR+0iKoSDvHZk=" | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $securePassword
+Connect-AzureRmAccount -ServicePrincipal -Credential $credential -TenantId "92832cfc-349a-4b12-af77-765b6f10b51f"
+
+$subscription_list = get-azurermsubscription
+$subscription_list
+
+<#
+
+Param (
+ [Parameter(Mandatory=$true)]
+ [String] $ApplicationDisplayName,
+
+ [Parameter(Mandatory=$true)]
+ [String] $SubscriptionId,
+
+ [Parameter(Mandatory=$true)]
+ [String] $CertPath,
+
+ [Parameter(Mandatory=$true)]
+ [String] $CertPlainPassword
+ )
+
+ Connect-AzAccount
+ Import-Module Az.Resources
+ Set-AzContext -Subscription $SubscriptionId
+
+ $CertPassword = ConvertTo-SecureString $CertPlainPassword -AsPlainText -Force
+
+ $PFXCert = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @($CertPath, $CertPassword)
+ $KeyValue = [System.Convert]::ToBase64String($PFXCert.GetRawCertData())
+
+ $ServicePrincipal = New-AzADServicePrincipal -DisplayName $ApplicationDisplayName
+ New-AzADSpCredential -ObjectId $ServicePrincipal.Id -CertValue $KeyValue -StartDate $PFXCert.NotBefore -EndDate $PFXCert.NotAfter
+ Get-AzADServicePrincipal -ObjectId $ServicePrincipal.Id 
+
+ $NewRole = $null
+ $Retries = 0;
+ While ($NewRole -eq $null -and $Retries -le 6)
+ {
+    # Sleep here for a few seconds to allow the service principal application to become active (should only take a couple of seconds normally)
+    Sleep 15
+    New-AzRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName $ServicePrincipal.ApplicationId | Write-Verbose -ErrorAction SilentlyContinue
+    $NewRole = Get-AzRoleAssignment -ObjectId $ServicePrincipal.Id -ErrorAction SilentlyContinue
+    $Retries++;
+ }
+
+ $NewRole
+
+#>
---
+++ AZURE SCRIPTS/Test1login.TempPoint.ps1
@@ -0,0 +1,27 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.159
+	 Created on:   	25/02/2019 19:27
+	 Created by:   	danny
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+$applicationId = "02e79480-acfb-4284-ac9e-7caaa8b244e8";
+$securePassword = "z5KMwA3DmvoeeSCvzoocvq5pHi78gsFR+0iKoSDvHZk=" | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $securePassword
+Connect-AzureRmAccount -ServicePrincipal -Credential $credential -TenantId "92832cfc-349a-4b12-af77-765b6f10b51f"
+
+# Fetching subscription list
+$Global:subscription_list = get-azurermsubscription
+$Global:subscription_list
+
+
+$azureVMDetails = get-azurermvm | Out-Null
+
+$azureVMDetails 
